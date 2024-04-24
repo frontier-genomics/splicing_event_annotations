8f1189294e6568b53919c2bf750cc311a3c1f19c
Rhett Marchant <rhett.marchant@gmail.com>
2024-04-24 00:08:56 +0000

    most positive strand events now supported

--- features/steps/eventAnnotate_test.py
+++ features/steps/eventAnnotate_test.py
@@ -25,9 +25,23 @@ def step_impl(context):
         type = context.input['type'],
         annotation_choice = context.input['annotation']
     )
+    context.annotation.get_annotations()
     context.start = context.annotation.matcher('start')
     context.end = context.annotation.matcher('end')
+    
+    print(context.input["start"])
+    print(context.input["end"])
+    print(context.start)
+    print(context.end)
+    
+    context.create_annotations = context.annotation.create_annotations(context.start, context.end)
+    
 
 @then(u'the result should be event {event} for transcript {transcript}')
 def step_impl(context, event, transcript):
-    raise NotImplementedError(u'the result should be event {event} for transcript {transcript}')
+    print(context.create_annotations['event'])
+    print(context.create_annotations['transcript'])
+    print(event)
+    print(transcript)
+    assert context.create_annotations['event'] == event
+    assert context.create_annotations['transcript'] == transcript
