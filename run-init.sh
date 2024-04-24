295d8c07715c383cd64c9e0904874c34fecb9ffe
Rhett Marchant <rhett.marchant@gmail.com>
2024-04-24 00:10:01 +0000

    removed bedtools sort

--- run-init.sh
+++ run-init.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 pip install -r requirements.txt
 pip install -r requirements-dev.txt
-
-#have to remove the weird chromosomes chrUn onwards
-bedtools sort -g chromosome_names.txt -i refseq_curated_introns.bed > refseq_curated_introns_sorted.bed 
-bedtools sort -g chromosome_names.txt -i refseq_mane_introns.bed > refseq_mane_introns_sorted.bed
