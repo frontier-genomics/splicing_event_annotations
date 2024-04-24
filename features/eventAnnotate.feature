23e921f9cf6608364351a5626805c0b7e8e17cf0
Rhett Marchant <60718636+rhettmarchant@users.noreply.github.com>
2024-04-24 10:11:57 +1000

    Support positive events (#1)
    * added exon and intron files for mane and curated
    * most positive strand splicing events now supported
    * updated devcontainer with ssh access
    * removed bedtools sort
    * added documentation powerpoint
    * removed unsorted bed and moved sorted bed to resources/bed

--- features/eventAnnotate.feature
+++ features/eventAnnotate.feature
@@ -1,34 +1,34 @@
 Feature: Annotate events in splicing data
 	To start with:
-		- I have a splicing event with chrom, start, end, strand, and type
-		- I have a transcript annotation file
-	
+	- I have a splicing event with chrom, start, end, strand, and type
+	- I have a transcript annotation file
+
 	Firstly:
-		- Annotate the starts
-		- Annotate the ends
-		- Find the intron overlapping the splicing event
-			1. Does the start match? (canonical, cryptics, skipping)
-				If yes: Does the end match? -> canonical
-				If no: Send to end_matcher
-			2. Does the end match? (cryptics, skipping)
-				If yes: Send to start_matcher
-			3. Is the start inside the intron?
-				If yes: Send to end_matcher
-			4. Is the end inside the intron?
-				If yes: Send to start_matcher
-			5. If all no: Send to an unmatched list
+	- Annotate the starts
+	- Annotate the ends
+	- Find the intron overlapping the splicing event
+	1. Does the start match? (canonical, cryptics, skipping)
+	If yes: Does the end match? -> canonical
+	If no: Send to end_matcher
+	2. Does the end match? (cryptics, skipping)
+	If yes: Send to start_matcher
+	3. Is the start inside the intron?
+	If yes: Send to end_matcher
+	4. Is the end inside the intron?
+	If yes: Send to start_matcher
+	5. If all no: Send to an unmatched list
 
 	Start Matcher:
-		Matcher with starts
-	
+	Matcher with starts
+
 	End Matcher:
-		Matcher with ends
+	Matcher with ends
 
 	Matcher (coordinates, annotations, start/end):
-		1. Canonical Splice-site
-		2. Same Transcript Splice-site (skipping, multi-skipping)
-		3. Different Transcript Splice-site (alternate-splicing, alternate-skipping)
-		4. Unannotated Splice-site (cryptic, intronic)
+	1. Canonical Splice-site
+	2. Same Transcript Splice-site (skipping, multi-skipping)
+	3. Different Transcript Splice-site (alternate-splicing, alternate-skipping)
+	4. Unannotated Splice-site (cryptic, intronic)
 
 	@wip
 	Scenario Outline: Annotate events in cortar data using RefSeq MANE Select Annotations
@@ -38,16 +38,16 @@ Feature: Annotate events in splicing data
 		Then the result should be event <event> for transcript <transcript>
 
 		Examples:
-			| chrom | start    | end      | strand | transcript     | event                                           | intron | comment                      | comment2                    |
-			| chr9  | 34646787 | 34647088 | +      | NM_000155.4    | canonical 1-2 splicing                          | 1      | galt_intron_1_merged_sjir_4  | canonical +                 |
-			| chr9  | 34646787 | 34647088 | +      | NM_000155.4    | intron 1 retention                              | 1      | galt_intron_1_merged_sjir_2  | intron retention +          |
-			| chr9  | 34647259 | 34647831 | +      | NM_000155.4    | exon 3-4 skipping                               | 2      | galt_intron_1_merged_sjir_9  | double exon skipping +      |
-			| chr9  | 34647259 | 34648114 | +      | NM_000155.4    | exon 3-4-5 skipping                             | 2      | galt_intron_1_merged_sjir_6  | triple exon skipping +      |
-			| chr9  | 34646920 | 34647088 | +      | NM_000155.4    | exonic cryptic donor @ -133                     | 1      | galt_intron_1_merged_sjir_0  | cryptic donor exonic +      |
-			| chr9  | 34646787 | 34647120 | +      | NM_000155.4    | exonic cryptic acceptor @ +32                   | 1      | galt_intron_1_merged_sjir_5  | cryptic acceptor exonic +   |
-			| chr9  | 34647259 | 34647474 | +      | NM_000155.4    | intronic cryptic acceptor @ -17                 | 2      | galt_intron_1_merged_sjir_12 | cryptic acceptor intronic + |
-			| chr9  | 34647259 | 34648091 | +      | NM_000155.4    | cryptic acceptor                                | 2      | galt_intron_1_merged_sjir_7  | funky 1                     |
-			| chr9  | 34647259 | 34648047 | +      | NM_000155.4    | cryptic acceptor                                | 2      | galt_intron_1_merged_sjir_8  | funky 2                     |
+			| chrom | start    | end      | strand | transcript  | type | event                                               | intron | comment                      | comment2                         |
+			| chr9  | 34646787 | 34647088 | +      | NM_000155.4 | sj   | canonical exon 1-2 splicing                         | 1      | galt_intron_1_merged_sjir_4  | canonical +                      |
+			| chr9  | 34646787 | 34647088 | +      | NM_000155.4 | ir   | intron 1 retention                                  | 1      | galt_intron_1_merged_sjir_2  | intron retention +               |
+			| chr9  | 34647259 | 34647831 | +      | NM_000155.4 | sj   | exon 3-4 skipping                                   | 2      | galt_intron_1_merged_sjir_9  | double exon skipping +           |
+			| chr9  | 34647259 | 34648114 | +      | NM_000155.4 | sj   | exon 3-4-5 skipping                                 | 2      | galt_intron_1_merged_sjir_6  | triple exon skipping +           |
+			| chr9  | 34646920 | 34647088 | +      | NM_000155.4 | sj   | intronic cryptic donor @ -133                       | 1      | galt_intron_1_merged_sjir_0  | cryptic donor exonic +           |
+			| chr9  | 34646787 | 34647120 | +      | NM_000155.4 | sj   | exonic cryptic acceptor @ +32                       | 1      | galt_intron_1_merged_sjir_5  | cryptic acceptor exonic +        |
+			| chr9  | 34647259 | 34647474 | +      | NM_000155.4 | sj   | intronic cryptic acceptor @ -17                     | 2      | galt_intron_1_merged_sjir_12 | cryptic acceptor intronic +      |
+			| chr9  | 34647259 | 34648091 | +      | NM_000155.4 | sj   | exon 3-4-5 skipping/intronic cryptic acceptor @ -23 | 2      | galt_intron_1_merged_sjir_7  | cryptic acceptor with skipping + |
+			| chr9  | 34647259 | 34648047 | +      | NM_000155.4 | sj   | exon 3-4-5 skipping/intronic cryptic acceptor @ -67 | 2      | galt_intron_1_merged_sjir_8  | cryptic acceptor with skipping + |
 # |       |          |          | +      |                |                                                 |        |                              | alternate intronic acceptor + |
 # |       |          |          | +      |                |                                                 |        |                              | alternate exonic donor +      |
 # |       |          |          | +      |                |                                                 |        |                              | alternate exonic acceptor +   |
--- src/eventAnnotateProcessor.py
+++ src/eventAnnotateProcessor.py
@@ -2,33 +2,8 @@ import pandas as pd
 import numpy as np
 
 class EventAnnotate:
-    """
-    Class representing an event annotation.
-
-    Attributes:
-        coordinates (dict): A dictionary containing the coordinates of the event.
-        annotation_choice (str): The choice of annotation to use.
-        annotation (pd.DataFrame): The annotation data.
-
-    Methods:
-        matcher: Matches the event coordinates with the annotation data.
-    """
 
     def __init__(self, chrom, start, end, strand, type, annotation_choice):
-        """
-        Initializes an EventAnnotate object.
-
-        Args:
-            chrom (str): The chromosome of the event.
-            start (int): The start position of the event.
-            end (int): The end position of the event.
-            strand (str): The strand of the event.
-            type (str): The type of the event.
-            annotation_choice (str): The choice of annotation to use.
-        
-        Raises:
-            ValueError: If the annotation_choice is invalid.
-        """
         self.coordinates = {
             'chrom': str(chrom),
             'start': np.int64(start),
@@ -38,27 +13,19 @@ class EventAnnotate:
         }
         self.annotation_choice = annotation_choice
 
+    def get_annotations(self):
         if self.annotation_choice == "refseq_mane":
-            self.annotation = pd.read_csv("resources/annotations/refseq_mane_introns_sorted.tsv", sep='\t', header=None)
+            self.annotation = pd.read_csv("resources/annotations/refseq_mane_introns_sorted.tsv", sep='\t')
+            self.exons = pd.read_csv("resources/annotations/refseq_mane_exons_sorted.tsv", sep='\t')
         elif self.annotation_choice == "refseq_curated":
-            self.annotation = pd.read_csv("resources/annotations/curated_introns_sorted.bed.gz", sep='\t', header=None)
+            self.annotation = pd.read_csv("resources/annotations/refseq_curated_introns_sorted.tsv", sep='\t')
+            self.exons = pd.read_csv("resources/annotations/refseq_curated_exons_sorted.tsv", sep='\t')
         else:
             raise ValueError("Invalid annotation choice. Please select either 'refseq_mane' or 'refseq_curated'.")
         
-        self.annotation.columns = ['chrom', 'start', 'end', 'intron', 'score', 'strand']
-
+        self.annotation.columns = ['chrom', 'start', 'end', 'transcript', 'intron', 'strand']
 
     def matcher(self, start_end, base = 1):
-        """
-        Matches the event coordinates with the annotation data.
-
-        Args:
-            start_end (str): The choice of start or end coordinate to match.
-            base (int, optional): The base to subtract from the start coordinate. Defaults to 1.
-        
-        Raises:
-            ValueError: If the start_end choice is invalid.
-        """
         if start_end not in ['start', 'end']:
             raise ValueError("Invalid start_end choice. Please select either 'start' or 'end'.")
         
@@ -69,13 +36,94 @@ class EventAnnotate:
         
         chrom = self.coordinates['chrom']
         
-        print(query)
-        print(self.annotation[start_end])
-        
         matching_rows = self.annotation[
             (self.annotation[start_end] == query) &
             (self.annotation['chrom'] == chrom)
         ]
         
+        matching_rows['start'] = matching_rows['start'] + base
         match = pd.DataFrame(matching_rows)
-        print(match)
+        return(match)
+        
+    def create_annotations(self, start_matches, end_matches):
+        if end_matches.empty and start_matches.empty:
+            return {'transcript': "unknown", 'event': "unannotated junctions"}
+        
+        elif start_matches.empty or end_matches.empty:
+            if end_matches.empty:
+                print("start matches")
+                end_matches = ""
+                start_intron = start_matches['intron'].iloc[0]
+                start_tx = start_matches['transcript'].iloc[0]
+                distance = self.coordinates['end'] - start_matches['end'].iloc[0]
+                return(self._name_event(start_tx, distance, self.coordinates['end'], "acceptor", start_matches))
+            
+            elif start_matches.empty:
+                print("end matches")
+                start_matches = ""
+                end_intron = end_matches['intron'].iloc[0]
+                end_tx = end_matches['transcript'].iloc[0]
+                distance = end_matches['start'].iloc[0] - self.coordinates['start']
+                return(self._name_event(end_tx, distance, self.coordinates['start'], "donor", end_matches))
+        
+        else:
+            start_intron = start_matches['intron'].iloc[0]
+            end_intron = end_matches['intron'].iloc[0]
+            start_tx = start_matches['transcript'].iloc[0]
+            end_tx = start_matches['transcript'].iloc[0]
+
+            if start_tx == end_tx:
+                if self.coordinates['type'] == "ir":
+                    event = f"intron {start_intron} retention"
+                    return {'transcript': start_tx, 'event': event}
+                elif start_intron == end_intron:
+                    event = f"canonical exon {start_intron}-{end_intron+1} splicing"
+                    return {'transcript': start_tx, 'event': event}
+                elif start_intron != end_intron:
+                    event = f"exon {'-'.join(str(i) for i in range(start_intron+1, end_intron+1))} skipping"
+                    return {'transcript': start_tx, 'event': event}
+
+            elif start_tx != end_tx:
+                    return {'transcript': "transcript mismatch", 'event': "unknown event"}
+                
+    def _name_event(self, tx, distance, start_end, splice_site_type, matches):
+        direction = "+" if distance > 0 else ""
+        within_tx_intron = self.annotation[(self.annotation['transcript'] == tx) &
+                        (self.annotation['start'] <= start_end) &
+                        (self.annotation['end'] >= start_end)
+        ]
+        
+        if not within_tx_intron.empty:
+            print("found an intronic variant")
+            print(f"{start_end} is between \n {within_tx_intron}")
+            print(within_tx_intron['intron'].iloc[0])
+            print(matches['intron'].iloc[0])
+            
+            if within_tx_intron['intron'].iloc[0] != matches['intron'].iloc[0]:
+                print("multiple introns involved")
+                start_intron = int(matches['intron'].iloc[0])
+                end_intron = int(within_tx_intron['intron'].iloc[0])
+                supp_event = f"exon {'-'.join(str(i) for i in range(start_intron+1, end_intron+1))} skipping/"
+                print(f"the second event is {supp_event}")
+                distance = start_end - within_tx_intron['end'].iloc[0]
+                direction = "+" if distance > 0 else ""
+            else:
+                supp_event = ""    
+                
+            event = f"{supp_event}intronic cryptic {splice_site_type} @ {direction}{distance}"
+            return {'transcript': tx, 'event': event}
+            
+        else:
+            print("didn't find an intronic variant")
+            within_tx_exon = self.exons[(self.exons['transcript'] == tx) &
+                            (self.exons['start'] <= start_end) &
+                            (self.exons['end'] >= start_end)
+            ]
+            if not within_tx_exon.empty:
+                print("found an exonic variant")
+                print(f"{start_end} is between {within_tx_exon}")
+                event = f"exonic cryptic {splice_site_type} @ {direction}{distance}"
+                return {'transcript': tx, 'event': event}
+            else:
+                print("no exonic variant either")
+                return {"something has gone wrong!?"}
